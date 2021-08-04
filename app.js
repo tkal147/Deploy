30ce69a10b5e9307d8b28b9af65be0c57f1b2923
tkal147 <frozenheartlucky@gmail.com>
2021-08-04 13:28:46 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+console.log('Server is running: ', PORT)
